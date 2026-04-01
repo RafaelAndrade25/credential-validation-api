f8560676733822e03e1adf44084b0b8ba6411a49
Rafael <rafael.andrade@grabgo.tech>
2026-04-01 18:21:07 -0300

    first-commit

---
+++ src/main/java/com/helptap/validation/CredentialValidationApiApplication.java
@@ -0,0 +1,13 @@
+package com.helptap.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialValidationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialValidationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helptap/validation/CredentialValidationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helptap.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialValidationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
